e67d4b1166e9ac43e1ae4a4621ac309d44424c97
Aparajita Fishman <aparajita@aparajita.com>
2013-11-21 18:32:26 +0000

    Run pep257 directly from built in python if possible.
    If pep257 can be imported, check_source is used to do the checking. Otherwise a metaclass is used to set cls.cmd to the executable to run.
    
    stdin is used now, depends on using my forked version at the moment.
    
    Added .sublimelinterrc to ensure it is parsed as python 3.

--- pep257.py
+++ pep257.py
@@ -10,17 +10,52 @@
 
 """Exports the Pep257 linter class."""
 
+import os
 
-from SublimeLinter.lint import highlight, PythonLinter
+# If possible, import pep257 and use it directly for better performance
+try:
+    from pep257 import check_source
+except ImportError:
+    check_source = None
+
+from SublimeLinter.lint import highlight, PythonLinter, Registrar
 
 
-class Pep257(PythonLinter):
+class Pep257Meta(Registrar):
+
+    """
+    Metaclass for Pep257 that dynamically sets the 'cmd' attribute.
+
+    If a linter can work both using an executable and built in code,
+    the best way to deal with that is to set the cmd class attribute
+    during class construction. This allows the linter to take advantage
+    of the rest of the SublimeLinter machinery for everything but run().
+
+    """
+
+    def __init__(cls, name, bases, attrs):
+        super().__init__(name, bases, attrs)
+
+        # If pep257 could not be imported, use the executable
+        if check_source is None:
+            setattr(cls, 'cmd', ('pep257@python', '-'))
+        else:
+            setattr(cls, 'cmd', None)
+
+
+class Pep257(PythonLinter, metaclass=Pep257Meta):
 
     """Provide an interface to the pep257 python script."""
 
     language = 'python'
-    cmd = 'pep257@python'
     regex = r'^.+?:(?P<line>\d+):(?P<col>\d+): (?P<error>.+)'
     default_type = highlight.WARNING
     line_col_base = (1, 0)
-    tempfile_suffix = '.py'
+
+    def run(self, cmd, code):
+        """Run pep257 on the source and return the output."""
+        if check_source is None:
+            return super().run(cmd, code)
+        else:
+            errors = check_source(code, os.path.basename(self.filename))
+            return '\n'.join([str(e) for e in errors])
