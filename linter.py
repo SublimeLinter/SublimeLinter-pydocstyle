c1daa6780721503efc65d2c006831a85d53bd9c3
Aparajita Fishman <aparajita@aparajita.com>
2013-12-16 12:43:06 -0800

    Added * arg placeholder.

--- linter.py
+++ linter.py
@@ -20,7 +20,7 @@ class PEP257(PythonLinter):
     """Provides an interface to the pep257 python module/script."""
 
     syntax = 'python'
-    cmd = ('pep257@python', '-')
+    cmd = ('pep257@python', '*', '-')
     regex = r'^.+?:(?P<line>\d+):(?P<col>\d+): (?P<message>.+)'
     default_type = highlight.WARNING
     line_col_base = (1, 0)  # pep257 uses one-based line and zero-based column numbers
